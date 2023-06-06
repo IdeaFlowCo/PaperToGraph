36612bc63c4fef738530e3ecdae10f92628f81e7
RPC <phillip@vspn.ltd>
2023-06-06 16:18:55 -0600

    Basic windowing with splitting by paragraph

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import openai
 
@@ -34,21 +35,51 @@ SYSTEM_MESSAGE_CONTENT = (
 SYSTEM_MESSAGE = {"role": "system", "content": SYSTEM_MESSAGE_CONTENT}
 
 
-def __fetch_parse(message:str, model="gpt-3.5-turbo"):
+def __fetch_parse(text:str, prev_context=None, model="gpt-3.5-turbo"):
     messages = [
-        SYSTEM_MESSAGE,
-        {"role": "user", "content": message}
+        SYSTEM_MESSAGE
     ]
 
-    result = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        max_tokens=3000,
-        temperature=1.2
+    if prev_context:
+        messages.append(
+            {"role": "assistant", "content": prev_context}
+        )
+
+    messages.append(
+        {"role": "user", "content": text}
     )
+
+    print(messages)
+
+    try:
+        result = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            max_tokens=3000,
+            temperature=1.2
+        )
+    except openai.error.RateLimitError:
+        # Wait a quarter second and try again
+        time.sleep(0.25)
+        return __fetch_parse(text, prev_context=prev_context, model=model)
+
     return result["choices"][0]["message"]["content"]
 
 
-def parse_with_gpt(message: str, model="gpt-3.5-turbo"):
-    parsed = __fetch_parse(message, model=model)
+def __split_to_size(text:str):
+    # Split by paragraphs first
+    chunked_text = list(filter(lambda x : x != '', text.split('\n\n')))
+    return chunked_text
+
+
+def parse_with_gpt(text: str, model="gpt-3.5-turbo"):
+    if len(text) <= 3000:
+        parsed = __fetch_parse(text, model=model)
+        return parsed
+    
+    text_chunks = __split_to_size(text)
+    parsed = None
+    for chunk in text_chunks:
+        parsed = __fetch_parse(chunk, prev_context=parsed, model=model)
+
     return parsed
