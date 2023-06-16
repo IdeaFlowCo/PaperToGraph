4ff5ba6be277dbdc637484fa716649841befa3d6
RPC <phillip@vspn.ltd>
2023-06-15 18:16:56 -0600

    Use new normalized_name property for identifying Neo4j entities

--- save.py
+++ save.py
@@ -3,12 +3,17 @@ Functions for saving parsed data to Neo4j.
 '''
 
 import json
-from re import sub
+from re import escape, sub
 
 from neo4j import GraphDatabase
 from neo4j.time import DateTime
 
 from utils import log_msg
+
+
+def normalize_entity_name(ent_name):
+    # Just convert to lowercase for now
+    return ent_name.lower()
 
 
 def sanitize_relationship_name(relationship):
@@ -26,13 +31,15 @@ def sanitize_relationship_name(relationship):
 # Function to create an entity in the database if it doesn't exist
 def create_entity_if_not_exists(driver, name, timestamp):
     with driver.session() as session:
-        result = session.run("MATCH (ent:Entity {name: $name}) RETURN count(ent) AS count", name=name)
+        normalized_name = normalize_entity_name(name)
+        result = session.run("MATCH (ent:Entity {normalized_name: $name}) RETURN count(ent) AS count", name=normalized_name)
         count = result.single()["count"]
         if count == 0:
             session.run(
                 # Note: have to call datetime() on created_at here to avoid getting localdatetime types by default
-                "CREATE (:Entity {name: $name, created_at: datetime($created_at)})", 
+                "CREATE (:Entity {name: $name, normalized_name: $normalized_name, created_at: datetime($created_at)})", 
                 name=name,
+                normalized_name=normalized_name,
                 created_at=timestamp,
             )
             log_msg(f"Entity '{name}' created in the database.")
@@ -43,20 +50,22 @@ def create_entity_if_not_exists(driver, name, timestamp):
 # Function to create a named relationship between two entities if it doesn't exist
 def create_relationship_if_not_exists(driver, ent1_name, relationship_name, ent2_name, timestamp):
     with driver.session() as session:
+        n_ent1_name = normalize_entity_name(ent1_name)
+        n_ent2_name = normalize_entity_name(ent2_name)
         result = session.run(
-            "MATCH (ent1:Entity {name: $ent1_name})-[r:%s]->(ent2:Entity {name: $ent2_name}) RETURN count(r) AS count"
+            "MATCH (ent1:Entity {normalized_name: $ent1_name})-[r:%s]->(ent2:Entity {normalized_name: $ent2_name}) RETURN count(r) AS count"
             % (relationship_name),
-            ent1_name=ent1_name,
-            ent2_name=ent2_name,
+            ent1_name=n_ent1_name,
+            ent2_name=n_ent2_name,
         )
         count = result.single()["count"]
         if count == 0:
             session.run(
-                "MATCH (ent1:Entity {name: $ent1_name}), (ent2:Entity {name: $ent2_name}) "
+                "MATCH (ent1:Entity {normalized_name: $ent1_name}), (ent2:Entity {normalized_name: $ent2_name}) "
                 # Note: have to call datetime() on created_at here to avoid getting localdatetime types by default
                 "CREATE (ent1)-[r:%s {created_at: datetime($created_at)}]->(ent2)" % (relationship_name),
-                ent1_name=ent1_name,
-                ent2_name=ent2_name,
+                ent1_name=n_ent1_name,
+                ent2_name=n_ent2_name,
                 created_at=timestamp,
             )
             log_msg(f"Relationship '{relationship_name}' created between '{ent1_name}' and '{ent2_name}'.")
